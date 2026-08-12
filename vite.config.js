c678d61123f4f08cc6b8cd0ba4d9016948b3120d
mustaphaaliyu520 <musyaphaaliyu520@gmail.com>
2026-08-12 08:00:13 +0100

    Add source code and components

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/campus-guard/', // Must match your GitHub repo name exactly
+  base: '/campus-guard/', // e.g., if repo URL is github.com/user/my-app, use '/my-app/'
 })
